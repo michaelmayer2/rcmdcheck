88a6c89979387245fd3a4d8c00ad579deece4ee9
Michael Mayer <michael.mayer@posit.co>
2024-04-23 12:38:04 +0000

    reformat code and other changes

--- test.R
+++ test.R
@@ -1,44 +1,80 @@
 library(rcmdcheck)
 library(remotes)
 library(tinytex)
-if(length(tinytex::tl_pkgs)==0) { tinytex::install_tinytex()}
 
-if (!require(pak,quietly = TRUE)) {
-  install.packages("pak", repos = sprintf(
-    "https://r-lib.github.io/p/pak/stable/%s/%s/%s",
-    .Platform$pkgType,
-    R.Version()$os,
-    R.Version()$arch
-  )) 
+if (length(tinytex::tl_pkgs()) == 0) {
+  tinytex::install_tinytex(bundle = "TinyTeX")
+}
+
+if (length(tinytex::tl_pkgs()) <= 104) {
+  tinytex::install_tinytex(bundle = "TinyTeX", force = TRUE)
 }
 
 options(pak.no_extra_messages = TRUE)
 library(pak)
 
-myrepo="https://packagemanager.posit.co/cran/latest/"
-#mybinrepo="https://packagemanager.posit.co/cran/__linux__/focal/latest/"
-#options(repos=c(CRAN=mybinrepo))
-packages<-available.packages()
-packages<-as.data.frame(packages)$Package
-packages<-c("tidyverse","clustermq")
+myrepo = "https://cran.r-project.org/"
+options(repos = c(CRAN = myrepo))
+options(repos = BiocManager::repositories())
 
+av_packages <- as.data.frame(available.packages())
 
-deps_test<-remotes::package_deps(packages,dependencies=c("Imports","LinkingTo")) #repos
+testing_packages = c("knitr", "dplyr", "testthat")
 
-deps_download<-remotes::package_deps(packages,dependencies=c("Imports","LinkingTo","Suggests")) #repos
+packages <- c("clustermq")
 
-deps_install<-paste0(deps_download$package,"@",deps_download$available)
-deps_testing<-paste0(deps_test$package,"@",deps_test$available)
+deps_testing <- unique(c(packages, unlist(
+  tools::package_dependencies(
+    packages,
+    which = c("Imports", "LinkingTo"),
+    recursive = TRUE
+  )
+)))
+
+deps_install <-
+  unique(unlist(tools::package_dependencies(
+    c(deps_testing, packages),
+    which = c("Imports", "LinkingTo", "Suggests", "Enhances")
+  )))
+
+deps_install <-
+  unique(c(
+    packages,
+    testing_packages,
+    unlist(tools::package_dependencies(
+      c(deps_testing, packages), which = c("Imports", "LinkingTo")
+    ))
+  ))
+
+deps_install <-
+  paste0(av_packages[av_packages$Package %in% deps_install,]$Package, "@", 
+            av_packages[av_packages$Package %in% deps_install,]$Version)
+
+deps_testing <-
+  paste0(av_packages[av_packages$Package %in% deps_testing,]$Package, "@", 
+            av_packages[av_packages$Package %in% deps_testing,]$Version)
 
 #.libPaths("/tmp/libs")
-callr::r_vanilla(pak::pkg_install(deps_install,"libs"),repos=options()$repos,libPath="~/libs")
+callr::r_vanilla(
+  pak::pkg_install(deps_install, "./libs"),
+  repos = options()$repos,
+  libPath = "./libs"
+)
 
-r<-options()$repos
-options(repos=c(CRAN=myrepo))
+pk <-
+  pak::pkg_download(deps_testing, dest_dir = "pkgs", platform = "source")
 
-pak::pkg_download(deps_testing,dest_dir="pkgs",platform="source")
-options(repos=r)
 
-oldlib<-.libPaths()
-.libPaths("~/libs")
-rcmdcheck::rcmdcheck(path="~/pkgs/src/contrib/tidyverse")
+libpath = paste0(getwd(), "/libs")
+#for (i in seq(nrow(pk))) {
+
+for (i in 2) {
+  print(pk[["package"]][[i]])
+  
+  rcmdcheck::rcmdcheck(
+    path = pk$fulltarget[[i]],
+    libpath = libpath,
+    quiet = FALSE,
+    env = c("_R_CHECK_FORCE_SUGGESTS_" = 0)
+  )
+}
